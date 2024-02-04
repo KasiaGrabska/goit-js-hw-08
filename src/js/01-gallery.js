4cbd0d9edad816f6129ce0544730f240541e0ac4
KasiaGrabska <143888097+KasiaGrabska@users.noreply.github.com>
2024-02-04 15:51:24 +0100

    Install npm simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Opisany w dokumentacji
+import SimpleLightbox from 'simplelightbox';
+// Dodatkowy import stylów
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const listItems = galleryItems
+  .map(
+    galleryItem =>
+      `<li class="gallery__item">
+            <a class="gallery__link" href="${galleryItem.original}">
+                <img
+            class="gallery__image"
+            src="${galleryItem.preview}"
+            alt="${galleryItem.description}"
+              />
+            </a>
+    </li>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('afterbegin', listItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
